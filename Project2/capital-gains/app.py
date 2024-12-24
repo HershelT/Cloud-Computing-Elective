e7e42fb1112baaa79afe855b2521eee6e93337f5
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2024-12-24 14:26:12 +0200

    Fixing capital gains with new method of getting values and query string

--- Project2/capital-gains/app.py
+++ Project2/capital-gains/app.py
@@ -19,22 +19,36 @@ def capital_gains():
     filters = request.args.to_dict()
     stocks1_url = 'http://stocks1-a:8000/stocks'
     stocks2_url = 'http://stocks2:8000/stocks'
-
-    try:
-        if filters:
-           stocks1_url += "?"
-           stocks1_url += '&'.join([f"{key}={value}" for key, value in filters.items()])
-
-        if filters:
-           stocks2_url += "?"
-           stocks2_url += '&'.join([f"{key}={value}" for key, value in filters.items()])
-        stocks1 = requests.get(stocks1_url).json()
-        stocks2 = requests.get(stocks2_url).json()
-    except requests.exceptions.ConnectionError:
-      return jsonify({'error':'Could not connect to stocks services'}), 500
-    all_stocks = stocks1 + stocks2
-    total_gain = get_total_gain(all_stocks)
-    return jsonify(total_gain)
+    get_stock1 = True
+    get_stock2 = True
+    # check if portfolio is stock1 or stock2
+    if 'portfolio' in filters:
+        if filters['portfolio'] == 'stock1':
+            get_stock2 = False
+        elif filters['portfolio'] == 'stock2':
+            get_stock1 = False
+    # filter by number of shares and append to url
+    if get_stock1:
+        if 'numsharesgt' in filters:
+            stocks1_url += '&numsharesgt=' + filters['numsharesgt']
+        if 'numshareslt' in filters:
+            stocks1_url += '&numshareslt=' + filters['numshareslt']
+    elif get_stock2:
+        if 'numsharesgt' in filters:
+            stocks2_url += '&numsharesgt=' + filters['numsharesgt']
+        if 'numshareslt' in filters:
+            stocks2_url += '&numshareslt=' + filters['numshareslt']
+    
+    if get_stock1 and get_stock2:
+        response1 = requests.get(stocks1_url)
+        response2 = requests.get(stocks2_url)
+        response_all = response1.json() + response2.json()
+    elif get_stock1:
+        response_all = requests.get(stocks1_url).json()
+    else:
+        response_all = requests.get(stocks2_url).json()
+    
+    return jsonify({"capital gains": get_total_gain(response_all)}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
