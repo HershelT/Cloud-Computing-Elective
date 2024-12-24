95ddd04768b65c6d6f46e2db89e887fbe160e4e5
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2024-12-24 15:27:22 +0200

    MAJOR UPDATE: Fixed capital gains. The URL was the problem the whole time to connect to host ports. Fixed the issue

--- Project2/capital-gains/app.py
+++ Project2/capital-gains/app.py
@@ -3,27 +3,31 @@ import requests
 
 app = Flask(__name__)
 
-
+# Function to calculate the total gain for a list of stocks
 def get_total_gain(stocks, stock_database_url):
     total_gain = 0
     # Loop through each stock and get the /stock-value from the appropriate data source
     for stock in stocks:
+        # Get the stock value from the stock database
         response = requests.get(f"{stock_database_url}/stock-value/{stock['_id']}")
         if response.status_code == 200:
+            # If the response is valid, calculate the total gain
             total_gain += response.json()['stock value'] - (stock['purchase price'] * stock['shares'])
     return total_gain
 
-
+# Route to calculate the capital gains which takes in query parameters (if any)
 @app.route('/capital-gains', methods=['GET'])
 def capital_gains():
+    # Get the query parameters
     filters = request.args.to_dict()
-    stocks1_url = 'http://stocks1-a:8000'
-    stocks2_url = 'http://stocks2:8000'
+    stocks1_url = 'http://stocks1-a:8000/stocks'
+    stocks2_url = 'http://stocks2:8000/stocks'
+    #flags to check if we need to get stocks from stock1 or stock2
     get_stock1 = True
     get_stock2 = True
     total_gains_stock1 = 0
     total_gains_stock2 = 0
-    query_string = ''
+    query_string = '?'
     # check if portfolio is stock1 or stock2
     valid_query = True
     valid_queries = ['portfolio', 'numsharesgt', 'numshareslt']
@@ -51,14 +55,16 @@ def capital_gains():
           query_string += '&numshareslt=' + lt
         else:
           valid_query = False
+    
+    # if query is invalid, return error
     if not valid_query:
       return jsonify({"error": f"Invalid query {filters}"}), 400
     
     #See if we need to get stocks from stock1 or stock2
     if get_stock1:
-      stocks1_url += '/stocks' + query_string
+      stocks1_url += query_string
     elif get_stock2:
-      stocks2_url += '/stocks' + query_string
+      stocks2_url += query_string
     
     # get stocks from the appropriate data source
     # Then call get_total_gain to calculate the total gains
