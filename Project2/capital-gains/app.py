d9afcf56f1296b4042f969cfee27602652719181
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2024-12-24 14:41:20 +0200

    New updates to capital-gains. Handling stocks using /stock-value instead of stocks

--- Project2/capital-gains/app.py
+++ Project2/capital-gains/app.py
@@ -3,24 +3,26 @@ import requests
 
 app = Flask(__name__)
 
-def get_total_gain(stocks):
-    total_gain = 0
-    for stock in stocks:
-        current_value = float(stock['stock value'])
-        purchase_price = float(stock['purchase price'])
-        gain = current_value - purchase_price
-        total_gain += gain
-    return total_gain
 
+def get_total_gain(stocks, stock_database_url):
+    total_gain = 0
+    # Loop through each stock and get the /stock-value from the appropriate data source
+    for stock in stocks:
+        response = requests.get(f"{stock_database_url}/stock-value/{stock['_id']}")
+        if response.status_code == 200:
+            total_gain += response.json()['stock value'] - stock['purchase price'] * stock['shares']
+    return total_gain
 
 
 @app.route('/capital-gains', methods=['GET'])
 def capital_gains():
     filters = request.args.to_dict()
-    stocks1_url = 'http://stocks1-a:8000/stocks'
-    stocks2_url = 'http://stocks2:8000/stocks'
+    stocks1_url = 'http://stocks1-a:8000'
+    stocks2_url = 'http://stocks2:8000'
     get_stock1 = True
     get_stock2 = True
+    total_gains_stock1 = 0
+    total_gains_stock2 = 0
     # check if portfolio is stock1 or stock2
     if 'portfolio' in filters:
         if filters['portfolio'] == 'stock1':
@@ -39,16 +41,23 @@ def capital_gains():
         if 'numshareslt' in filters:
             stocks2_url += '&numshareslt=' + filters['numshareslt']
     
+    # get stocks from the appropriate data source
+    # Then call get_total_gain to calculate the total gains
     if get_stock1 and get_stock2:
-        response1 = requests.get(stocks1_url)
-        response2 = requests.get(stocks2_url)
-        response_all = response1.json() + response2.json()
+        response1 = requests.get(stocks1_url + '/stocks')
+        response2 = requests.get(stocks2_url + '/stocks')
+        total_gains_stock1 = get_total_gain(response1.json(), stocks1_url)
+        total_gains_stock2 = get_total_gain(response2.json(), stocks2_url)
     elif get_stock1:
-        response_all = requests.get(stocks1_url).json()
+        response1 = requests.get(stocks1_url).json()
+        total_gains_stock1 = get_total_gain(response1, stocks1_url)
     else:
-        response_all = requests.get(stocks2_url).json()
+        response2 = requests.get(stocks2_url).json()
+        total_gains_stock2 = get_total_gain(response2, stocks2_url)
     
-    return jsonify({"capital gains": get_total_gain(response_all)}), 200
+    total_gains = total_gains_stock1 + total_gains_stock2
+    return jsonify({"capital gains": total_gains}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
