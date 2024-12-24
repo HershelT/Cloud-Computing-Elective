e2802448ac2dacec0d985adc8027cf964cba823f
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2024-12-24 15:17:03 +0200

    Fixed url error in capital-gains

--- Project2/capital-gains/app.py
+++ Project2/capital-gains/app.py
@@ -10,7 +10,7 @@ def get_total_gain(stocks, stock_database_url):
     for stock in stocks:
         response = requests.get(f"{stock_database_url}/stock-value/{stock['_id']}")
         if response.status_code == 200:
-            total_gain += response.json()['stock value'] - stock['purchase price'] * stock['shares']
+            total_gain += response.json()['stock value'] - (stock['purchase price'] * stock['shares'])
     return total_gain
 
 
@@ -30,6 +30,7 @@ def capital_gains():
     if '?' in request.url:
       if not all(key in valid_queries for key in filters.keys()):
         valid_query = False
+      # filter by portfolio
       if 'portfolio' in filters:
         if filters['portfolio'] == 'stocks1':
           get_stock2 = False
@@ -52,25 +53,27 @@ def capital_gains():
           valid_query = False
     if not valid_query:
       return jsonify({"error": f"Invalid query {filters}"}), 400
+    
+    #See if we need to get stocks from stock1 or stock2
     if get_stock1:
-      stocks1_url += query_string
-    if get_stock2:
-      stocks2_url += query_string
+      stocks1_url += '/stocks' + query_string
+    elif get_stock2:
+      stocks2_url += '/stocks' + query_string
     
     # get stocks from the appropriate data source
     # Then call get_total_gain to calculate the total gains
     if get_stock1 and get_stock2:
-        response1 = requests.get(stocks1_url + '/stocks')
-        response2 = requests.get(stocks2_url + '/stocks')
-        total_gains_stock1 = get_total_gain(response1.json(), stocks1_url)
-        total_gains_stock2 = get_total_gain(response2.json(), stocks2_url)
+        response1 = requests.get(stocks1_url)
+        response2 = requests.get(stocks2_url)
+        total_gains_stock1 = get_total_gain(response1.json(), 'http://stocks1-a:8000')
+        total_gains_stock2 = get_total_gain(response2.json(), 'http://stocks2:8000')
     elif get_stock1:
         response1 = requests.get(stocks1_url).json()
-        total_gains_stock1 = get_total_gain(response1, stocks1_url)
+        total_gains_stock1 = get_total_gain(response1, 'http://stocks1-a:8000')
     else:
         response2 = requests.get(stocks2_url).json()
-        total_gains_stock2 = get_total_gain(response2, stocks2_url)
-    
+        total_gains_stock2 = get_total_gain(response2, 'http://stocks2:8000')
+    # Calculate the total gains from both
     total_gains = total_gains_stock1 + total_gains_stock2
     return jsonify({"capital gains": total_gains}), 200
 
