5cb6ec50f6bfee26242be7ec5ccd6739776eeba2
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2025-01-14 21:49:32 +0200

    Trying to figure out why not all services are running!

--- Project3/multi-service-app/capital-gains/app.py
+++ Project3/multi-service-app/capital-gains/app.py
@@ -76,7 +76,7 @@ def capital_gains():
     return jsonify({'total gains': total_gains_stock1}), 200
 
 if __name__ == '__main__':
-    app.logger.setLevel('INFO-CAPITAL-GAINS')
+    app.logger.setLevel('INFO')
     port = int(os.environ.get('PORT', 8080))
     print(f"Starting capital-gains service on port {port}")
     app.run(host='0.0.0.0', port=8080)
