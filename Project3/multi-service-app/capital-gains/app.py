8152ab4723ed12dcf7c6428b3619dbbbf1a8f231
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2025-01-15 18:06:36 +0200

    Trying to configure right ports

--- Project3/multi-service-app/capital-gains/app.py
+++ Project3/multi-service-app/capital-gains/app.py
@@ -79,5 +79,5 @@ if __name__ == '__main__':
     app.logger.setLevel('INFO')
     port = int(os.environ.get('PORT', 8080))
     print(f"Starting capital-gains service on port {port}")
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
