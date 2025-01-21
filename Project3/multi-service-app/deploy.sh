d87eaf0ef429cafe6e17f078f6fa2a7b4de52474
Hershel Thomas <hershel.thomas@post.runi.ac.il>
2025-01-21 14:53:31 +0200

    Updated deploy.sh to wait for pods to run

--- Project3/multi-service-app/deploy.sh
+++ Project3/multi-service-app/deploy.sh
@@ -24,5 +24,46 @@ kubectl apply -f .
 cd ..
 cd database
 kubectl apply -f .
+
+# Wait for all to be running:
+sleep 10
+
+# wait until all the Pods are in the Running state
+timeout=300
+interval=5 # Check every 5 seconds.
+elapsed=0
+
+function check_pods_running() {
+  # Get the count of pods that are NOT in the 'Running' state.
+  not_running_count=$(kubectl get pods --all-namespaces -o yaml |
+     yq '.items[] | select(.status.phase != "Running" and .status.phase != "Succeeded") | length' | wc -l)
+
+  echo "Pods not in 'Running' state: $not_running_count"
+
+  if [ "$not_running_count" -eq 0 ]; then
+    return 0 # All pods are running.
+  else
+    return 1 # There are pods not in the 'Running' state.
+  fi
+}
+
+all_pods_running=false
+while [ "$elapsed" -lt "$timeout" ]; do
+  if check_pods_running; then
+    echo "All pods are in the 'Running' state."
+    all_pods_running=true
+    break
+  fi
+  echo "Waiting for all pods to be in the 'Running' state..."
+  sleep "$interval"
+  elapsed=$((elapsed + interval))
+done
+
+if [ "$all_pods_running" = false ]; then
+  echo "Timed out waiting for all pods to be in the 'Running' state."
+  exit 1
+fi
+
 # Validate the deployment
 kubectl get all -n multi-service-app
+
